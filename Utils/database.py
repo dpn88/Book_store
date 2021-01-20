15000a7340ce6ac143c46a67e49c5c677a32e90b
dpn88 <70435626+dpn88@users.noreply.github.com>
2021-01-20 17:44:21 +0000

    Update database.py
    update to functions to load and save directly from the file

--- Utils/database.py
+++ Utils/database.py
@@ -1,29 +1,15 @@
 import csv
 """
-books stored in this file
+functions needed by main file to process data stored in books.csv
 """
-books_extra = []
 
-books = [
-    {
-        'name': 'Dune',
-        'author': 'Frank Herbert',
-        'read': False
-    },
-    {
-        'name': 'Pan Tadeusz',
-        'author': 'Adam Mickiewicz',
-        'read': False
-    },
-    {
-        'name': 'Hobbit',
-        'author': 'J.R.R. Tolkien',
-        'read': True
-    }
-]
+books = []
+# list populated by other functions and used to save data
+# fetched from the books.csv in the memory and to operate on the data
 
 
-def prompt_add_book():
+def prompt_add_book():  # adds a book to the books.csv file and prompts was the book read or not
+    read_books()
     name = input("Enter the Book title: ").capitalize()
     author = input("Enter the Books author: ").capitalize()
     read = (input("have you read the Book? y/n: ").lower())
@@ -31,9 +17,9 @@ def prompt_add_book():
         read = (input("have you read the Book? (y)es or (n)ot: ").lower())
     else:
         if read == 'y':
-            read = True
+            read = 'True'
         elif read == 'n':
-            read = False
+            read = 'False'
 
     books.append({
         'name': name,
@@ -41,32 +27,44 @@ def prompt_add_book():
         'read': read
     })
     print("Thank you! Your book is added")
+    save_books()
+    books.clear()
 
 
-def list_books():
-    for book in books:
-        if book['read'] is True:
+def list_books():   # fetches the data from books.csv na prints it out
+    read_books()
+    library = books
+    for book in library:
+        if book['read'] == 'True':
             read = 'read'
         else:
             read = 'not read'
         print(f'{book["name"]} by {book["author"]}, {read}')
+    books.clear()
 
 
-def prompt_read_book():
+def prompt_read_book():     # allows to mark a book as "read" and checks was the book already marked or not
+    read_books()
     name = input('\nWhat book are you looking for? ').lower()
     counter = []
     for book in books:
         if book['name'].lower() == name:
-            user_input = input(f"\n{name.capitalize()} by {book['author']} "
-                               f"- Would you like to mark it as read? If yes type 'y' ").lower()
-            if user_input == 'y':
-                book['read'] = True
+            if book['read'] == 'True':
+                print(f'Book {book["name"]} was marked as read already')
+            else:
+                user_input = input(f"\n{name.capitalize()} by {book['author']} "
+                                   f"- Would you like to mark it as read? If yes type 'y' ").lower()
+                if user_input == 'y':
+                    book['read'] = 'True'
             counter.append(book)
     if len(counter) == 0:
         print(f"{name} is not in your library")
+    save_books()
+    books.clear()
 
 
-def prompt_delete_book():
+def prompt_delete_book():   # deletes a book from the database
+    read_books()
     name = input('\nWhat book would you like to delete? ').lower()
     for i in range(len(books)):
         if books[i]['name'].lower() == name:
@@ -78,23 +76,21 @@ def prompt_delete_book():
                 break
     else:
         print(f"{name.capitalize()} is not in your library")
+    save_books()
+    books.clear()
 
 
-def read_extra_books():
-    with open('Utils/books.csv', 'r') as books_base:
-        reader = csv.DictReader(books_base)
-        counter = 0
+def read_books():   # function used by other functions to fetch data from database
+    with open('Utils/books.csv', 'r') as f:
+        reader = csv.DictReader(f)
         for line in reader:
             name = line['name']
             author = line['author']
             read = line['read']
             books.append({'name': name, 'author': author, 'read': read})
-            books_extra.append({'name': name, 'author': author, 'read': read})
-            counter += 1
-        print(f'{counter} added book(s)')
 
 
-def save_books():
+def save_books():   # function used by other functions to save data to database
     with open('Utils/books.csv', 'w') as f:
         writer = csv.DictWriter(f, fieldnames=['name', 'author', 'read'])
         writer.writeheader()
